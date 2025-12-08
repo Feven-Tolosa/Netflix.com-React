867a6c0042bc77065279d62d8d876f4000e341b0
Feven-Tolosa <tolosafeven0@gmail.com>
2025-12-08 17:43:41 +0300

    unwanted row commented

--- Netflix/src/request.jsx
+++ Netflix/src/request.jsx
@@ -1,4 +1,4 @@
-const API_KEY = "8fb17e5bb4251925f123b5655ac066b9";
+const API_KEY = '8fb17e5bb4251925f123b5655ac066b9'
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
@@ -6,8 +6,8 @@ const requests = {
   fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fatchActionMovies: `discover/movie?api_key=${API_KEY}&with_genres=28`,
   fatchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-  fatchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-  fatchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  // fatchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  // fatchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
   fatchDocumenteriesMovies: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-};
-export default requests;
+}
+export default requests
